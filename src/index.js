97110df7f6af6db7dd3870135a5e64aa22324506
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-03 19:31:28 -0500

    cards layout and api connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.css';
+import emptyHeartIcon from './img/empty_heart_icon.png';
+
+const listElement = document.getElementById('items-list');
+const apiURL = 'http://api.tvmaze.com/search/shows?q=';
+
+const getMealsByIngredient = async (topic) => {
+  const res = await fetch(`${apiURL}${topic}`);
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
+
+const render = (list, parentElement) => {
+  let listItems = '';
+  list.forEach((item) => {
+    listItems += `\
+    <li class="card">\
+      <img class="card-img" src=${item.show.image.medium}>
+      <div class="card-info-container">
+        <p class="card-title">${item.show.name}</p>
+        <div class="card-likes-container">
+          <img class="card-heart" src=${emptyHeartIcon}>
+          <p class="card-likes-text">5 likes</p>
+        </div>
+      </div>
+      <div class="card-buttons-container">
+        <button class="card-button">Comments</button>
+        <button class="card-button">Reservation</button>
+      </div>    
+    </li>`;
+  });
+  parentElement.innerHTML = listItems;
+};
+
+window.addEventListener('load', async () => {
+  const itemsToRender = await getMealsByIngredient('girls');
+  render(itemsToRender, listElement);
+});
