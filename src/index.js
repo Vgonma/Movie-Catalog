72d43e6b455c7be03283f5889f3a20f6d65e99d0
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-03 20:49:46 -0500

    converted functions to module methods

--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
 import './style.css';
-import emptyHeartIcon from './img/empty_heart_icon.png';
+import TvmazeConnection from './modules/TvmazeConnection.js';
+import Render from './modules/Render.js';
 
 const listElement = document.getElementById('items-list');
-const apiURL = 'http://api.tvmaze.com/search/shows?q=';
-
-const getMealsByIngredient = async (topic) => {
-  const res = await fetch(`${apiURL}${topic}`);
-  const data = await res.json();
-  console.log(data);
-  return data;
-};
-
-const render = (list, parentElement) => {
-  let listItems = '';
-  list.forEach((item) => {
-    listItems += `\
-    <li class="card">\
-      <img class="card-img" src=${item.show.image.medium}>
-      <div class="card-info-container">
-        <p class="card-title">${item.show.name}</p>
-        <div class="card-likes-container">
-          <img class="card-heart" src=${emptyHeartIcon}>
-          <p class="card-likes-text">5 likes</p>
-        </div>
-      </div>
-      <div class="card-buttons-container">
-        <button type="button" class="card-button">Comments</button>
-        <button type="button" class="card-button">Reservation</button>
-      </div>    
-    </li>`;
-  });
-  parentElement.innerHTML = listItems;
-};
+const tvMazeConnection = new TvmazeConnection();
+const render = new Render(listElement);
 
 window.addEventListener('load', async () => {
-  const itemsToRender = await getMealsByIngredient('girls');
-  render(itemsToRender, listElement);
+  const itemsToRender = await tvMazeConnection.getMoviesByTopic('planet');
+  render.show(itemsToRender);
 });
---
+++ src/modules/Render.js
@@ -0,0 +1,29 @@
+import emptyHeartIcon from '../img/empty_heart_icon.png';
+
+export default class Render {
+  constructor(parentElement) {
+    this.parentElement = parentElement;
+  }
+
+  show = (list) => {
+    let listItems = '';
+    list.forEach((item) => {
+      listItems += `\
+      <li id="${item.show.id}" class="card">\
+        <img class="card-img" src=${item.show.image.medium}>
+        <div class="card-info-container">
+          <p class="card-title">${item.show.name}</p>
+          <div class="card-likes-container">
+            <img class="card-heart" src=${emptyHeartIcon}>
+            <p class="card-likes-text">5 likes</p>
+          </div>
+        </div>
+        <div class="card-buttons-container">
+          <button type="button" class="card-button">Comments</button>
+          <button type="button" class="card-button">Reservation</button>
+        </div>    
+      </li>`;
+    });
+    this.parentElement.innerHTML = listItems;
+  };
+}
