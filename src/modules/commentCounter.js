4a4dc0b3c26ae53554093e619cb6414c2d4d450a
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-05 09:45:31 -0600

    Add module commentCounter.js

---
+++ src/modules/commentCounter.js
@@ -0,0 +1,7 @@
+export default function commentCounter(DOMContainer) {
+  let count = 0;
+  for (let i = 0; i < DOMContainer.children.length; i += 1) {
+    if (DOMContainer.children[i].classList.contains('comment')) count += 1;
+  }
+  return count;
+}
